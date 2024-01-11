3102f06529d6e7b51099ef0f538bc335d8c74f2d
Sourav <ssourav474474@gmail.com>
2024-01-11 20:23:02 +0530

    Frontend added

---
+++ JavaScript/index.js
@@ -0,0 +1,36 @@
+
+var a = 10;
+var b = 20;
+console.log("The sum is = "+(a+b))
+
+let c = 10;
+let d = 40;
+console.log("The sum is = "+(c+d))
+
+var x = 20;
+let y = 30;
+const z = 30;
+
+let avg = (x+y+z)/3;
+console.log("Average = "+ (avg))
+console.log("Hello Javascript")
+
+let e = 20;
+if(e%2== 0){
+    console.log(e+" is even");
+}
+else{
+    console.log(e+"is odd");
+}
+
+for(let i = 1; i <=5; i++){
+    console.log(i);
+}
+
+let k = 3
+for(let j=1; j<=10; j++){
+    console.log(k+" * "+j+" = "+(k*j));
+}
+
+
+
---
+++ JavaScript/index1.js
@@ -0,0 +1,30 @@
+let a = [10, 25, 20, 40.5, 50, 60]
+console.log(a.length);
+a.unshift(); // added to the first index
+a.pop(); // remove element from the last
+a.sort(); // it will sort the order
+a.push(70); // add element in thi last
+a.reverse();
+console.log(a);
+
+
+// Creating an array using literals
+let myArray = [1, 2, 'apple', 'orange', true];
+
+// Displaying elements of the array
+console.log("Elements of the array are:" + myArray);
+
+// Creating an array using the array constructor
+let myArray1 = new Array(3); // Creates an array with length 3
+
+// Adding elements to the array
+myArray1[0] = 'apple';
+myArray1[1] = 'banana';
+myArray1[2] = 'orange';
+
+console.log("Elements of the array:" + myArray1);
+
+
+
+
+
