b581eded7a549160dec054025536945e4442f161
Sourav <ssourav474474@gmail.com>
2024-01-25 19:42:29 +0530

    typecasting added

---
+++ Python/typecasting.py
@@ -0,0 +1,12 @@
+x = 4
+y= 6
+
+x_string = str(x)
+y_string = str(y)
+
+final_string = x_string + y_string
+print(final_string)
+
+celsius = float(input("Enter temperature in celsius: "))
+fahrenheit = (celsius * 1.8) + 32
+print("Temperature in fahrenheit is: " , fahrenheit)
