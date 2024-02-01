8f3d2382cc694acc06ce09426fc6f1209b5bce76
Sourav <ssourav474474@gmail.com>
2024-02-01 16:35:53 +0530

    function added in javascript

---
+++ NPM/function.js
@@ -0,0 +1,14 @@
+//function definition
+function greet(){
+    console.log("Sourav");
+}
+// function call
+greet();
+
+// function with parameters
+function greet1(a,b) {
+    return (a+b);
+}
+let x = greet1(2,3);
+console.log(x); //5
+console.log(greet1(24,3)); //27
