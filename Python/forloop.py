9cb86d9b35bfbb3051f5c45b5e0104b337870b1f
Sourav <ssourav474474@gmail.com>
2024-02-23 16:14:47 +0530

    loops added in Python

---
+++ Python/forloop.py
@@ -0,0 +1,11 @@
+
+for i in range(1, 11, 2):
+    print(i, "Hello, world")
+
+for _ in range(5):
+    print("Sourav")
+
+list = [2, 5, 9, 10, 1]
+
+for i in list:
+    print(i)
---
+++ Python/while.py
@@ -0,0 +1,24 @@
+#Print even number using while loop
+
+i = 2
+while i <= 28:
+    print(i)
+    i += 2
+
+z = 10
+while z == 20:
+    print("hello")
+#No output
+
+x=4
+y=0
+
+while x > 0:
+    x -= 1
+    y += 1
+
+    if x == y:
+        continue
+    else:
+        print(x,y)
+#Output : 3 1  , 1 3,  0 4
